28683fbeb6df6208278bcdbae6aa2ac9d251fbe1
Shehan Chamudith Udakumbura <110516558+ShehanChamudith@users.noreply.github.com>
2024-04-20 13:52:29 +0530

    SideBar routing done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,28 +3,28 @@ import { SideBar } from './components/SideBar';
 import { Admin } from './pages/Admin';
 import { Bill } from './pages/Bill';
 import Login from './pages/Login';
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className='h-screen'>
-      
-      <Router>
-          {/* <div>
-            <SideBar/>
-          </div> */}
-        <Routes>
-        <Route path='/' element={<SideBar/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/bill' element={<Bill/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/admin' element={<Admin/>}/>
-        </Routes>
-      </Router>
-      
-    </div>
+    <Router>
+      <div className='flex h-screen'>
+        <SideBar />
+        <div className='flex-1'>
+          <Routes>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/bill' element={<Bill/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/admin' element={<Admin/>}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+{/* <Route path='/' element={<SideBar/>}/> */}
--- front-end/src/pages/Bill.jsx
+++ front-end/src/pages/Bill.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Bill = () => {
   return (
     <div>
-        <h1 className='text-3xl text-center text-white'>This is Bill Page</h1>
+        <h1 className='text-3xl text-center text-black'>This is Bill Page</h1>
     </div>
   )
 }
